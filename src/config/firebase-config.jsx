3f15fd0cdeb52fff9c64238db27e4581cbd9f6c2
Tiwami626 <tevirungu@gmail.com>
2024-05-16 18:37:40 +0300

    Made changes to firebase-config and gitignore files

--- src/config/firebase-config.jsx
+++ src/config/firebase-config.jsx
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "xxxxxxxxxx",
+  authDomain: "keja-a010d.firebaseapp.com",
+  projectId: "keja-a010d",
+  storageBucket: "keja-a010d.appspot.com",
+  messagingSenderId: "982433031888",
+  appId: "1:982433031888:web:4c5361365c0c273465e0d5",
+  measurementId: "G-NEGRHLQJP1",
 };
 
 const app = initializeApp(firebaseConfig);
